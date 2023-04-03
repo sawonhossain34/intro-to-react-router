b7d47822c267528773a11b8322239d188c9f17c6
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-03 11:44:47 +0600

    first commit and react router home page outlet add

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+
+// const router = createBrowserRouter ([
+//   {
+//     path: '/',
+//     element : <App></App>
+//   },
+//   {
+//     path: '/about',
+//     element : <About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element : <Contact></Contact>
+//   }
+// ])
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <Home></Home>,
+    children : [
+      {
+        path : "about",
+        element : <About></About>
+      },
+      {
+        path : "contact",
+        element : <Contact></Contact>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
